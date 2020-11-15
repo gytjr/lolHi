9647505e4323bb8ee94177fe4356ceb739cb4759
gytjr <gytjr321@naver.com>
2020-11-15 21:51:59 +0900

    게시물 상세페이지, 로그인 아이디 찾기 관련 인터셉터 경로 수정

--- src/main/java/com/sbs/example/lolHi/config/WebMvcConfig.java
+++ src/main/java/com/sbs/example/lolHi/config/WebMvcConfig.java
@@ -29,8 +29,9 @@ public class WebMvcConfig implements WebMvcConfigurer {
 		// 로그인 없이도 접속할 수 있는 URI 전부 기술
 		registry.addInterceptor(needToLoginInterceptor).addPathPatterns("/**").excludePathPatterns("/")
 				.excludePathPatterns("/resource/**").excludePathPatterns("/usr/home/main").excludePathPatterns("/usr/member/login")
+				.excludePathPatterns("/usr/member/findLoginid").excludePathPatterns("/usr/member/doFindLoginid")
 				.excludePathPatterns("/usr/member/doLogin").excludePathPatterns("/usr/member/join")
 				.excludePathPatterns("/usr/member/doJoin").excludePathPatterns("/usr/article-*/list")
-				.excludePathPatterns("/usr/article/detail").excludePathPatterns("/error");
+				.excludePathPatterns("/usr/article-*/detail").excludePathPatterns("/error");
 	}
 }
